2ebd262d25d65ff33338d190e29c3777e13010a3
Oyebamiji Micheal Tomiwa <oyebamijimicheal10@gmail.com>
2023-01-14 05:14:21 +0100

    Define student profile schema

--- CSCResult/CSCResult/Models/StudentProfile.cs
+++ CSCResult/CSCResult/Models/StudentProfile.cs
@@ -6,5 +6,10 @@ namespace CSCResult.Models
 {
     public class StudentProfile
     {
+        public int ProfileId { get; set; }
+        public string MatricNo { get; set; }
+        public string Name { get; set; }
+        public string Level { get; set; }
+        public string AdmissionCategory { get; set; }
     }
 }
