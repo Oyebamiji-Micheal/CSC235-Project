542fb2ba6c23ed296e248cb65208377272e7b5d2
Oyebamiji Micheal Tomiwa <oyebamijimicheal10@gmail.com>
2023-01-15 01:54:04 +0100

    Add 'public' access specifier to BaseViewModel

--- CSCResult/CSCResult/ViewModels/BaseViewModel.cs
+++ CSCResult/CSCResult/ViewModels/BaseViewModel.cs
@@ -6,7 +6,7 @@ using System.Text;
 
 namespace CSCResult.ViewModels
 {
-    class BaseViewModel : INotifyPropertyChanged
+    public class BaseViewModel : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
