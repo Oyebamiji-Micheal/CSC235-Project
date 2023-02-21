f9587b0591fd4319fff46ea3571a1183a1eb73e5
Oyebamiji Micheal Tomiwa <oyebamijimicheal10@gmail.com>
2023-02-21 21:47:03 +0100

    Create StudentCoursesService.cs

---
+++ CSCResult/CSCResult/Services/StudentCoursesService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Text;
+using Firebase.Database;
+using Firebase.Database.Query;
+using System.Linq;
+
+using Xamarin.Forms.Xaml;
+using CSCResult.Models;
+using System.Collections.ObjectModel;
+using CSCResult.Views;
+
+namespace CSCResult.Services
+{
+    public class StudentCoursesService
+    {
+        FirebaseClient client;
+        public StudentCoursesService()
+        {
+            client = new FirebaseClient("https://cscresultapp-default-rtdb.firebaseio.com");
+        }
+    }
+}
