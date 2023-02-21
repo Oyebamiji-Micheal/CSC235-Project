34289a70366739d2c25e40bc4b1c35a8430a15cf
Oyebamiji Micheal Tomiwa <oyebamijimicheal10@gmail.com>
2023-02-21 21:13:46 +0100

    Create ICourseService.cs

---
+++ CSCResult/CSCResult/Services/Interfaces/ICourseService.cs
@@ -0,0 +1,16 @@
+﻿using CSCResult.Models;
+using CSCResult.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSCResult.Services.Interfaces
+{
+    public interface ICourseService
+    {
+        Task<bool> AddOrUpdateCourse(StudentCoursesModel studentCoursesModel);
+        Task<bool> DeleteCourse(string key);
+        Task<List<StudentCoursesModel>> GetAllCourses();
+    }
+}
