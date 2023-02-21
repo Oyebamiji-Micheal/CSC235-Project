1fe2b6bc053a7d64a41522bb7f710471abdea813
Oyebamiji Micheal Tomiwa <oyebamijimicheal10@gmail.com>
2023-02-21 21:11:58 +0100

    Create StudentCoursesModel.cs

---
+++ CSCResult/CSCResult/Models/StudentCoursesModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSCResult.Models
+{
+    public class StudentCoursesModel
+    {
+        public string Key { get; set; }
+        public string CourseCode { get; set; }
+        public string CourseDescription { get; set; } 
+        public string MatricNo{ get; set; }
+        public string CourseUnit { get; set; } // It is assumed that no calculation is done on this attribute
+        public string AdminEmail { get; set; }
+    }
+}
