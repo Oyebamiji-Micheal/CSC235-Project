a4641ab353351dcd2a2b679f5b40c0df9d2f392e
Oyebamiji Micheal Tomiwa <oyebamijimicheal10@gmail.com>
2023-02-21 21:45:44 +0100

    Update BaseViewModel.cs

--- CSCResult/CSCResult/ViewModels/BaseViewModel.cs
+++ CSCResult/CSCResult/ViewModels/BaseViewModel.cs
@@ -8,9 +8,39 @@ namespace CSCResult.ViewModels
 {
     public class BaseViewModel : INotifyPropertyChanged
     {
-        public event PropertyChangedEventHandler PropertyChanged;
 
-        public void OnPropertyChanged([CallerMemberName] string name = null) =>
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        #region Properties
+        private bool _isBusy;
+        public bool IsBusy
+        {
+            get => _isBusy;
+            set => SetProperty(ref _isBusy, value);
+        }
+        #endregion
+
+        protected bool SetProperty<T>(ref T backingStore, T value,
+     [CallerMemberName] string propertyName = "",
+     Action onChanged = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(backingStore, value))
+                return false;
+
+            backingStore = value;
+            onChanged?.Invoke();
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            var changed = PropertyChanged;
+            if (changed == null)
+                return;
+
+            changed.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
     }
 }
