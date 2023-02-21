7fb9a4086bf1ad158060c5ad5869ed5fb1795c53
Oyebamiji Micheal Tomiwa <oyebamijimicheal10@gmail.com>
2023-02-21 21:51:13 +0100

    Create CourseList.xaml.cs

---
+++ CSCResult/CSCResult/Views/CourseList.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CSCResult.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CourseList : ContentPage
+    {
+        public CourseList()
+        {
+            InitializeComponent();
+        }
+        async public void AddCourse_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new AddUpdateCourse());
+        }
+    }
+}
