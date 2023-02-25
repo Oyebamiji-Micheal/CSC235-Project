a8a911c537b61a45f3a831b7913c307bfdd220b3
Oyebamiji Micheal Tomiwa <oyebamijimicheal10@gmail.com>
2023-02-25 07:46:32 +0100

    Update App.xaml.cs

--- CSCResult/CSCResult/App.xaml.cs
+++ CSCResult/CSCResult/App.xaml.cs
@@ -14,13 +14,16 @@ namespace CSCResult
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            // MainPage = new AppShell();
             // MainPage = new NavigationPage(new AddLoginDetails());
             // MainPage = new NavigationPage(new AddStudentProfileDetails());
+            MainPage = new NavigationPage(new StudentLogin());
             // MainPage = new NavigationPage(new CourseList());
             // MainPage = new NavigationPage(new AddCourse());
             // MainPage = new NavigationPage(new StudentPage());
+            // MainPage = new NavigationPage(new StudentResultPage());
             DependencyService.Register<ICourseService, StudentCoursesService>();
+            DependencyService.Register<IAdminService, AdminService>();
         }
 
         protected override void OnStart()
